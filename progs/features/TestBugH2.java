5d1678a2d619756378207813f9df7d79e6ce9b1e
AliHassanL12 <liaqqahl@gmail.com>
2024-03-17 22:52:35 +0000

    small error

--- progs/features/TestBugH2.java
+++ progs/features/TestBugH2.java
@@ -2,6 +2,6 @@ class TestBugH2 {
 	public static void main(String[] args) {
 		int x = 0;
 		do {
-			System.out.println("Should print even if x is 0);
+			System.out.println("Should print even if x is 0");
 		} while (x<0);
 }
